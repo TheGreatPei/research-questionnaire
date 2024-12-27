4d3af59042bbb046a8a886b0f87c3a28ed124236
TheGreatPei <147324074+TheGreatPei@users.noreply.github.com>
2024-12-27 14:42:38 +0800

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: 'https://zaoaaopqdsrisgcryys.supabase.co',
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOi'
+  }
+};
+
+module.exports = nextConfig;
